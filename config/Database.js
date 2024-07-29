e31f9329fb082d3a1845e048ae3640d4077b0b2c
Muhammad Rafi Al Qodri <72026404+mraq123@users.noreply.github.com>
2024-07-29 21:23:25 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 import mysql2 from "mysql2";
 
-const db = new Sequelize("b1sdysgf7rivanz35bdl", "ui0crxc7ow8jz97f", "f1XGtNEUlMY3fyCd632t", {
-  host: "b1sdysgf7rivanz35bdl-mysql.services.clever-cloud.com",
+const db = new Sequelize("kbsxkffm_siami", "kbsxkffm_bsi", "Bismillahsidang", {
+  host: "103.67.79.123",
   dialect: "mysql",
   dialectModule: mysql2,
   logging: false,
